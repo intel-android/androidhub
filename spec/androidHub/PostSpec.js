2aa2255c15d1c3d2a296318e93af4eaf2c2ff310
Aaron Egaas <me@aaronegaas.com>
2015-08-26 14:50:44 -0700

    updated to support svg, added imagemagick and test image sizes

--- spec/androidHub/PostSpec.js
+++ spec/androidHub/PostSpec.js
@@ -5,6 +5,10 @@ var fs     = require("fs");
 var jade   = require("jade");
 var yaml   = require("js-yaml");
 var request = require("request");
+var path = require("path");
+var root = path.resolve(process.cwd());
+var imagemagick = require("imagemagick");
+var mime = require("mime");
 
 describe("Posts", function() {
   var glob = require("glob");
@@ -24,7 +28,7 @@ describe("Posts", function() {
 });
 
 function test_post(filename) {
-  it("filename is a string", function () {
+  it("filename is a string", function() {
     expect(filename).toBeString();
   });
 
@@ -35,30 +39,30 @@ function test_post(filename) {
     expect(fileInput).toBeString();
   });
 
-  describe(post + " is a jade file with yaml frontmatter", function () {
+  describe(post + " is a jade file with yaml frontmatter", function() {
     var parts     = fileInput.split('---');
     var yamlInput = parts[1];
     var jadeInput = parts[2];
     var data,template;
 
-    beforeEach(function () {
+    beforeEach(function() {
       data = yaml.safeLoad(yamlInput);
       template = jade.compile(jadeInput, {filename: filename});
     });
 
-    describe("author", function () {
+    describe("author", function() {
       // author comes from the path
       var author = filename.split('/')[1];
 
-      it('is required', function () {
+      it('is required', function() {
         expect(author).toBeString();
       });
 
-      it('is not too long (20 characters)', function () {
+      it('is not too long (20 characters)', function() {
         expect(author.length).toBeLessThan(20);
       });
 
-      describe('has matching author.yaml', function () {
+      describe('has matching author.yaml', function() {
         var yamlPath = "data/authors/" + author + ".yaml";
 
         it("file exists", function (done) {
@@ -78,48 +82,36 @@ function test_post(filename) {
       });
     });
 
-    describe('yaml data', function () {
+    describe('yaml data', function() {
       it('yaml parses into object', function() {
         // TODO parse into yaml and inspect object for required vars
         expect(data).toBeObject();
       });
 
-      describe("title field", function () {
-        it('is required', function () {
+      describe("title field", function() {
+        it('is required', function() {
           expect(data.title).toBeString();
         });
 
-        it('is not too long (200 characters)', function () {
+        it('is not too long (200 characters)', function() {
           expect(data.title.length).toBeLessThan(200);
         });
       });
 
-      // describe("date key", function () {
-      //   it('is required', function () {
-      //     expect(data.date).toBeString();
-      //   });
-
-      //   // moment(date) throws a deprecation warning
-      //   // see https://github.com/moment/moment/issues/1407 for more info
-      //   it('date parses by moment.js to a valid date', function () {
-      //     expect(data.date).toBeValidDate();
-      //   });
-      // });
-
       describe("categories key", function() {
-        it('is an array of strings', function () {
+        it('is an array of strings', function() {
           expect(data.categories).toBeArrayOfStrings();
         });
 
-        it('at least 1 category', function () {
+        it('at least 1 category', function() {
           expect(data.categories.length).toBeGreaterThan(0);
         });
 
-        it('but not too many (3 max)', function () {
+        it('but not too many (3 max)', function() {
           expect(data.categories.length).toBeLessThan(4);
         });
 
-        it('contains valid categories', function () {
+        it('contains valid categories', function() {
           var validCategories = ['ideate', 'design', 'build', 'launch'];
 
           for (var i = 0; i < data.categories.length; i++) {
@@ -136,16 +128,30 @@ function test_post(filename) {
 
       describe("remote image", function() {
         var response = null;
+        var filename = null;
+        var extension = null;
+
+        var download = function(uri, filename, callback){
+          request.head(uri, function(err, res, body){
+            response = res;
+            extension = mime.extension(res.headers['content-type']);
+
+            request(uri).pipe(fs.createWriteStream(filename + '.' + extension)).on('close', callback);
+          });
+        };
 
         beforeAll(function(done) {
-          // jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
-          request.head(data.heroimage, function(err, res, body) {
-            if (err) {
-              done.fail(err);
-            } else {
-              response = res;
-              done();
-            }
+          jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+          filename = root + '/tmp/' + Math.random().toString().split('.').pop();
+          download(data.heroimage, filename, function () {
+            done();
+          });
+        });
+
+        afterAll(function(done) {
+          fs.unlink(filename + '.' + extension, function (err) {
+            if (err) throw err;
+            done();
           });
         });
 
@@ -154,18 +160,49 @@ function test_post(filename) {
         });
 
         it('has an image content-type', function() {
-          expect(['image/jpeg', 'image/png']).toContain(response.headers['content-type']);
+          expect(['image/jpeg', 'image/png', 'image/svg']).toContain(response.headers['content-type']);
         });
 
-        it('is smaller than 2M bytes', function() {
-          expect(parseInt(response.headers['content-length'])).toBeLessThan(2000000);
+        it('is smaller than 5MB', function() {
+          expect(parseInt(response.headers['content-length'])).toBeLessThan(5000000);
+        });
+
+        describe('heroimage dimensions', function() {
+          var image = null;
+          beforeAll(function (done) {
+            imagemagick.identify(filename + '.' + extension, function(err, features){
+              if (err) throw err;
+              image = features;
+              done();
+            });
+          });
+
+          it('has a height larger than 100px', function() {
+            expect(image.height).toBeGreaterThan(99);
+          });
+
+          it('has a width larger than 640px', function() {
+            expect(image.width).toBeGreaterThan(639);
+          });
         });
       });
     });
 
 
-    it('jade parses into a function', function() {
-      expect(template).toBeFunction();
+    describe('jade', function () {
+      it('parses into a function', function() {
+        expect(template).toBeFunction();
+      });
+
+      xdescribe('external resources', function () {
+        xit('are local resources', function () {
+
+        });
+
+        xit('are referencing the local author\'s assets dir');
+
+        xit('resolve to a valid resource');
+      });
     });
 
   });
