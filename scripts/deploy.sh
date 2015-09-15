3c325b981f4448efe4f7644c8277127380de166a
Aaron Egaas <me@aaronegaas.com>
2015-09-14 21:39:29 -0700

    ignore deploy_key, use rsync to save speed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 deploy_key
 ssh-add deploy_key
 
-scp -i deploy_key -r public/* ${DEPLOY_USER}@${DEPLOY_IP}:${DEPLOY_PATH}
+rsync -i deploy_key --delete -r public/* ${DEPLOY_USER}@${DEPLOY_IP}:${DEPLOY_PATH}
