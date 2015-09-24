e5ed617fb5b6a21a619f3106f30bb20bac8c61be
Adam Argyle <atom@argyleink.com>
2015-09-24 15:14:39 -0700

    new author fix required github handle, also no longer create first post

--- scripts/new-author.js
+++ scripts/new-author.js
@@ -58,6 +58,10 @@ var validUrl = function(input) {
 var checkIfNewAuthor = function(input) {
   var done = this.async();
 
+  if (!required(input)) {
+    return done("github username is required");
+  }
+
   fs.exists(path.resolve(root + '/data/authors/' + input + '.yaml'), function(exists) {
     if (exists) {
       done("this username already exists. Pick another.");
@@ -78,7 +82,7 @@ var questions = [
     type    : 'input',
     message : 'What is your *github* username? *',
     name    : 'github',
-    validate: required && checkIfNewAuthor
+    validate: checkIfNewAuthor
   },
   {
     type    : 'input',
@@ -125,23 +129,9 @@ inquirer.prompt(questions, function( answers ) {
     console.log('This will populate the authors page as well as pull your information into your posts.');
   });
 
-  //create posts directory
-  fs.mkdirSync(path.resolve(root + '/posts/' + user), 0755);
-  fs.mkdirSync(path.resolve(root + '/posts/' + user + '/library'), 0755);
-
-  //copy in the template.jade into the directory
-  var template = fs.readFileSync(path.resolve(root + '/posts/template.jade'), 'utf8');
-  var templatePath = path.resolve(root + '/posts/' + user);
-  template = template.replace("\n_ignore:    true", '');
-  fs.writeFile(templatePath + '/first_post.jade', template, function (err) {
-    if (err) throw err;
-    console.log('first_post.jade was created and placed in ' + templatePath);
-    console.log();
-    console.log('Next steps:');
-    console.log('1. run: "npm run post" to create a new post');
-    console.log('2. write your awesome content');
-    console.log('3. submit a pull request to http://github.com/intelandroid/androidhub.');
-    console.log();
-    console.log('Thanks for contributing!');
-  });
+  console.log();
+  console.log('Next steps:');
+  console.log('1. run: "npm run post" to create a new post');
+  console.log();
+  console.log('Thanks for contributing!');
 });
