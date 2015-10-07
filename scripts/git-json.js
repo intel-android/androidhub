16355da9b3d4a19a7480413ec4f7954a7a54a7c2
Aaron Egaas <aegaas@deloitte.com>
2015-10-06 18:46:43 -0700

    cleanup of git-json script

--- scripts/git-json.js
+++ scripts/git-json.js
@@ -8,48 +8,19 @@ if (!which('git')) {
 }
 
 mkdir('-p', 'public');
-// #!/usr/bin/env bash
-//
-// # Use this one-liner to produce a JSON literal from the Git log:
-//
-// #!/bin/bash
-// FILES=$@
-// arrFILES=(${FILES[@]})
-// length=${#arrFILES[@]}
-// last=${arrFILES[$length-1]}
-//
-// echo "{"
-// for f in $FILES
-// do
-//   git --no-pager log -1 --pretty=format:"\"$f\": {%n  \"commit\": \"%H\",%n  \"author\": \"%an <%ae>\",%n  \"date\": \"%ad\",%n  \"message\": \"%s\"%n}" $f
-//
-//   if [[ "$f" != "$last" ]]
-//   then
-//     echo ","
-//   fi
-//   # take action on each file. $f store current file name
-// done
-// echo ""
-// echo "}"
-
 var json = [];
 var files = glob.sync('posts/*/*.jade');
 
 var object = {};
 for(var i=0; i<files.length; i++) {
   var file = files[i];
-  echo(file);
-
   var formatter = '"{%n  \\"commit\\": \\"%H\\",%n  \\"author\\": \\"%an <%ae>\\",%n  \\"date\\": \\"%ad\\",%n  \\"message\\": \\"%s\\"%n}"';
   var command = 'git --no-pager log -1 --pretty=format:' + formatter + ' ' + file;
-  echo(command);
   var git = exec(command, {async: false, silent: true});
   object[file] = JSON.parse(git.output);
 }
 
 var content = JSON.stringify(object);
-
-echo(content);
 
 content.to('public/posts-git.json');
 exit(0);
