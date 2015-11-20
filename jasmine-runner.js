fbad09118062b9483095ed100dffad72214e9f45
Sierra Hsieh <sihsieh@deloitte.com>
2015-11-20 11:36:41 -0800

    set timeout interval to 30 seconds for slower macbook airs running tests

--- jasmine-runner.js
+++ jasmine-runner.js
@@ -4,6 +4,7 @@ var noop = function() {};
 
 var jrunner = new Jasmine();
 jrunner.configureDefaultReporter({print: noop});    // remove default reporter logs
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 jasmine.getEnv().addReporter(new SpecReporter());   // add jasmine-spec-reporter
 jrunner.loadConfigFile();                           // load jasmine.json configuration
 jrunner.execute();
