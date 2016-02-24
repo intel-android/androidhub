d6108b186911c0fd9f0c9fc1575c78255cdaf30e
Aaron Egaas <me@aaronegaas.com>
2016-02-23 20:53:34 -0800

    rewrite internal root focused urls to /en/ for localization features. Currently copies .html files, eventually will move them once they're ready.

---
+++ scripts/prep_l18n.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# cleanup
+rm -rf public/en/posts
+rm -rf public/en/*.html
+
+# Create an /en/ folder in the root 
+mkdir -p public/en/posts
+
+# clean out all images in posts
+rm -rf public/posts/**/library
+
+# and move the following items into the /en/ folder:
+# android-field-guide
+# posts (folder)
+# index.html
+# 404.html
+# about.html
+# authors.html
+# commit.html
+# feed.html
+
+cp -R public/*.html public/en
+cp -R public/posts/* public/en/posts
+
+# For links in the HTML files to work for all languages without any changes after translation:
+# use root-relative paths for links to assets.
+# use relative (not root-relative) paths to link to other HTML files.
+
+find -X public/en -name '*.html' | xargs sed -i -e 's/\<a href=\"\//\<a href=\"\/en\//g'
+find -X public/en -name '*.html-e' | xargs rm -f
